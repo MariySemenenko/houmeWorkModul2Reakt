3decc7a3103a376aaf5c399698296e18ae8dac1e
MariySemenenko <mashyni92@gmail.com>
2023-05-29 14:30:31 +0300

    finish

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import _ from 'lodash';
 
 // controlled form
 
@@ -6,11 +7,16 @@ import React from "react";
 constructor(props) {
     super(props);
     this.state = {
-        searchResult: ''
+        searchResult: '',
+        selectResult: ''
     }
 
     this.hendleSearch = this.hendleSearch.bind(this);
      this.displaySearchResult = this.displaySearchResult.bind(this);
+     this.buldTypeOptions = this.buldTypeOptions.bind(this);
+     this.hendleSearch = this.hendleSearch.bind(this);
+     this.handleReset = this.handleReset.bind(this);
+     
 }
 
 
@@ -19,26 +25,51 @@ this.setState({ searchResult: event.target.value });
 
 }
 
+handleSelect(event) {
+    this.setState({ searchResult: event.target.value });
+}
+
+handleReset() {
+this.setState({searchResult: '', selectResult: ''});
+}
+
 displaySearchResult() {
-    const {searchResult} = this.state;
+    const {searchResult, selectResult} = this.state;
     const { products } = this.props;
-const filteredProducts = products.filter((product) => {
+
+    console.log({searchResult, selectResult});
+
+const filteredProducts = products.filter(({type, name}) => {
     const searchResultLower = searchResult.toLowerCase();
-    const productTypeLower = product.type.toLowerCase(); 
-    return productTypeLower.includes(searchResultLower);
+    const productTypeLower = type.toLowerCase(); 
+    return productTypeLower.includes(searchResultLower)
+    && name === selectResult ;
 });
   
-return 
-    filteredProducts.map((product) => 
+return filteredProducts.map((product) => 
     
-         <div>Type: {product.type} || Amount: {product.amount} || Currency: {product.currency}</div>      
+         <div key={product.id}>Type: {product.type} || Amount: {product.amount} || Currency: {product.currency}</div>      
     
 );
 
 }
 
+//I get uniq products
+buldTypeOptions() {
+    const { products } = this.props;
+    // search uniq type by lodash
+    const uniqProductsByTypes = _.uniqBy(products, 'type');
+const uniqProductTypes = _.map(uniqProductsByTypes, 'type');
+
+return uniqProductTypes.map((type, index) =>
+<option key={`type-${type}-${index}`} value={type}>{type}</option>
+);
+
+}
+
 render() {
-const {searchResult} = this.state;
+const {searchResult, selectResult} = this.state;
+
 
 
 return(
@@ -48,8 +79,20 @@ return(
 placeholder="Search"
 value={searchResult}
 onChange={this.hendleSearch}
-
 />
+
+<label>
+          Select product type
+          <select name="type" onChange={this.handleSelect} value={selectResult}>
+          <option value="" disabled>
+              ...
+            </option>
+
+            {this.buldTypeOptions()}
+          </select>
+        </label>
+
+         <button onClick={this.handleReset} style={{marginLeft: 30}}>Reset</button>
 
 <div style={{margin: 30}}>
     <h1>List of product</h1>
